ea383fc812826f4953a08639df7103e67f48ec48
Вадим Мещеряков <painassasin@gmail.com>
2021-02-08 07:16:52 +0500

    Set bills as self

--- atm.py
+++ atm.py
@@ -1,30 +1,28 @@
 
 
 class ATM:
-    BILLS = dict.fromkeys([10, 50, 100, 200, 500, 1000, 2000, 5000], 0)
 
     def __init__(self):
         self.balance = 0
+        self.bills = dict.fromkeys((10, 50, 100, 200, 500, 1000, 2000, 5000), 0)
 
     def put(self, bill, count):
-        if bill in self.BILLS:
-            self.BILLS[bill] += count
+        if bill in self.bills:
+            self.bills[bill] += count
             self.balance += bill * count
 
     def get(self, amount) -> dict:
         result_bills = dict()
-        values = list(self.BILLS.keys())
-        values.sort(reverse=True)
-        for value in values:
+        for value in reversed(self.bills.keys()):
             count = amount // value
-            num = count if count <= self.BILLS[value] else self.BILLS[value]
+            num = count if count <= self.bills[value] else self.bills[value]
             amount -= num * value
             if num:
                 result_bills[value] = num
 
         if not amount:
             for k, v in result_bills.items():
-                self.BILLS[k] -= v
+                self.bills[k] -= v
             self.balance -= sum([k*v for k, v in result_bills.items()])
             return result_bills
 
