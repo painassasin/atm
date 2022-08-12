444522c0237b0ae5ae7144fc6bfa1e4fe629512a
Vadim Meshcheryakov <painassasin@icloud.com>
2022-08-12 15:02:36 +0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="atm",
+    version="0.0.1",
+    author="Vadim Meshcheryakov",
+    author_email="painassasin@icloud.com",
+    description="Atm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/painassasin/atm",
+    packages=find_packages(exclude=['tests']),
+    python_requires='>=3.9',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
